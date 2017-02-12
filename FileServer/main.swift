06a936ac329714c6edfcc16252418a37e3e7bfff
Jonathan Cardasis <joncardasis@gmail.com>
2017-02-12 15:26:03 -0500

    Initial Commit

---
+++ FileServer/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  FileServer
+//
+//  Created by Jon Cardasis on 2/12/17.
+//  Copyright © 2017 Jonathan Cardasis. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
