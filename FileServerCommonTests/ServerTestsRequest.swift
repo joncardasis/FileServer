9ae0568280ada45372b8ce17f2aaef05995df935
Jonathan Cardasis <joncardasis@gmail.com>
2017-03-22 17:46:31 -0400

    Added Cathage and Quick frameworks

--- FileServerCommonTests/ServerTestsRequest.swift
+++ FileServerCommonTests/ServerTestsRequest.swift
@@ -18,7 +18,7 @@ class ServerTestsRequest: XCTestCase {
             "User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36",
             "Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
             "Accept-Encoding: gzip, deflate, sdch, br",
-            "Accept-Language: en-US,en;q=0.8"
+            "Accept-Language:en-US,en;q=0.8"
             ].joined(separator: "\r\n") + "\r\n"
         
         let headerData = header.data(using: .utf8)!
@@ -27,6 +27,10 @@ class ServerTestsRequest: XCTestCase {
         XCTAssertEqual(testRequest.method, .get)
         XCTAssertEqual(testRequest.path, "/images/coolImage.jpeg")
         
+        XCTAssertEqual(testRequest.body["User-Agent"], "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36")
+        XCTAssertEqual(testRequest.body["Accept"], "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8")
+        XCTAssertEqual(testRequest.body["Accept-Encoding"], "gzip, deflate, sdch, br")
+        XCTAssertEqual(testRequest.body["Accept-Language"], "en-US,en;q=0.8")
     }
     
     
